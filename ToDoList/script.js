ab22787f10f949e660c90d6f7e113e8dc73d05ce
IAmNoNothing <allo290216@gmail.com>
2025-12-15 15:40:05 +0200

    save/load

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,7 +1,48 @@
 const cardTemplate = document.getElementById("card-template");
 const main = document.getElementById("main");
-const cardButton = document.getElementById("add-card-button");
+let cardButton = document.getElementById("add-card-button");
+
+function save() {
+    localStorage.setItem("data", JSON.stringify(getCardData()));
+}
 
 cardButton.addEventListener("click", () => {
     main.insertBefore(cardTemplate.content.cloneNode(true), cardButton);
+    save();
 });
+
+function onCardRemoved(img) {
+    img.parentElement.parentElement.remove();
+    save();
+}
+
+function getCardData() {
+    let result = [];
+    for (const child of main.children) {
+        if (child.id != "add-card-button") {
+            const name = child.querySelector(".name").value;
+            const description = child.querySelector(".description").value;
+            result.push([name, description]);
+        }
+    }
+    return result;
+}
+
+function setCardData(data) {
+    main.innerHTML = '';
+
+    data.forEach(([name, description]) => {
+        const node = cardTemplate.content.cloneNode(true);
+        node.querySelector(".name").value = name;
+        node.querySelector(".description").value = description;
+        main.appendChild(node);
+    });
+
+    main.appendChild(cardButton);
+}
+
+const data = localStorage.getItem("data");
+
+if (data !== null) {
+    setCardData(JSON.parse(data));
+}
